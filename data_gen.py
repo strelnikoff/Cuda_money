799c035723af251d09b4f20049cb56bd51c2b073
Oleg <oleglip48@gmail.com>
2016-12-10 13:40:12 +0300

    Теперь гениерит датасет на основе всех файлов из папки, плюс небельшой багфикс

--- data_gen.py
+++ data_gen.py
@@ -8,6 +8,12 @@ import cv2 as cv
 import numpy as np
 from os import listdir
 from os.path import isfile, join
+import os
+
+
+class names():
+    global img_save_num
+    global img_directory
 
 
 # Загружаем изображение
@@ -21,11 +27,13 @@ def get_points(image):
     img = cv.medianBlur(image, 5)
     gimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     circles = cv.HoughCircles(gimg, cv.HOUGH_GRADIENT, 1, \
-            160, param1=60, param2=40, minRadius=70, maxRadius=150)  # если плохо находит монеты менять эти параметры 
+            160, param1=60, param2=40, minRadius=140, maxRadius=180)  # если плохо находит монеты менять эти параметры 
     circles = np.uint16(np.around(circles))
 #    print(circles)
     for i in circles[0, :]:
         cv.circle(img, (i[0], i[1]), i[2], (40, 150, 40), 2)
+    height, width, channels = img.shape    
+    img = cv.resize(img, (int(width/3), int(height/3)))
     cv.imshow('Circles',img)
     cv.waitKey(0)
     cv.destroyAllWindows()
@@ -37,15 +45,18 @@ def get_images_by_point(image, circles):
     croped_images = []
     height, width, channels = image.shape
     for c in circles[0, :]:
-        w = int(c[2] * 2 * 2)
-        h = int(c[2] * 2 * 2)
+        w = int(c[2] * 2 * 3)
+        h = int(c[2] * 2 * 3)
         x = int(c[0] - w / 2)
         y = int(c[1] - h / 2)
-        if ((y + h) > height) or ((x + w) > width):
-            next
+        if ((y + h) > height) or ((x + w) > width) or ( y < 0) or ( x < 0):
+            print('w=', w, 'h=', h, 'x=', x, 'y=', y)
+            continue
+            
         crop_img = image[y:y + h, x:x + w]
+        print('w=', w, 'h=', h, 'x=', x, 'y=', y)
 #        cv.imshow('cr', crop_img)
-        crop_img = cv.resize(crop_img, (200, 200))
+        crop_img = cv.resize(crop_img, (512, 512))
 #        cv.imshow('cr', crop_img)
 #        cv.waitKey(0)
 #        cv.destroyAllWindows()
@@ -54,7 +65,7 @@ def get_images_by_point(image, circles):
 
 
 # уродуем каждую монетку
-def get_more_img(image):
+def get_more_img(image, names):
     after_filter = []
     # вертим пикчу по 20 градусов за раз, так как получаемое изображение
     # в 2 раза больше то черных полос точно не будет 
@@ -69,11 +80,16 @@ def get_more_img(image):
         x = int(image_center[0] / 2)
         y = int(image_center[1] / 2)
         crop_center = img[y - 5:y + h + 5, x - 5:x + w + 5]  # по размеру кружка + 5 пикс с каждой стороны
-        after_filter.append(cv.resize(crop_center, (256, 256)))
+        cv.imwrite(names.img_directory+str(names.img_save_num)+'.jpg', cv.resize(crop_center, (512, 512)))
+        names.img_save_num = names.img_save_num + 1
         crop_right_up=img[y + 5:y + h + 10, x + 5:x + w + 10]  # двигаем вправо вверх
-        after_filter.append(cv.resize(crop_right_up, (256, 256)))
+        cv.imwrite(names.img_directory+str(names.img_save_num)+'.jpg', cv.resize(crop_right_up, (512, 512)))
+        names.img_save_num = names.img_save_num + 1
+        #after_filter.append(cv.resize(crop_right_up, (512, 512)))
         crop_left_down=img[y - 10:y + h - 5, x - 10:x + w - 5]  # двигаем влево вниз
-        after_filter.append(cv.resize(crop_left_down, (256, 256)))
+        cv.imwrite(names.img_directory+str(names.img_save_num)+'.jpg', cv.resize(crop_left_down, (512, 512)))
+        names.img_save_num = names.img_save_num + 1
+        #after_filter.append(cv.resize(crop_left_down, (512, 512)))
     return after_filter
 
 def save_images_from_list(images, PATH):
@@ -83,12 +99,14 @@ def save_images_from_list(images, PATH):
         cv.imwrite(name, img)
 
 if __name__ == "__main__":
-    image_location = "front.jpg"  # путь к изображению
-    save_way = "output\\"  # папка куда сохранять датесет, должен быть пустым или существовать
-    image=get_image(image_location)
-    circles=get_points(image)
-    images=get_images_by_point(image, circles)
-    out=[]
-    for img in images:
-        out.extend(get_more_img(img))
-    save_images_from_list(out, save_way)
+    x = names()
+    x.img_save_num = 1
+    x.img_directory = '1r\\'
+    imgs = os.listdir(x.img_directory)
+    out = []
+    for img_name in imgs:
+        image = get_image(x.img_directory+img_name)
+        circles=get_points(image)
+        images=get_images_by_point(image, circles)
+        for img in images:
+            out.extend(get_more_img(img, x))
